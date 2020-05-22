42350e551b8c8f88f37aa467e4407cd15e325569
Blockman_ <65733896+Blockman02135@users.noreply.github.com>
2020-05-22 12:37:44 +0300

    Update EssentialsX.py

--- EssentialsX.py
+++ EssentialsX.py
@@ -15,16 +15,21 @@ async def on_ready():
 async def help(ctx, page):
 	if page == '1':
     embed = discord.Embed( #Показываем что это ембед
-    title = 'Тут ваш заговолок' ,  #Загаловок
-    description = 'Тут ваше описание'  , #Описание
+    title = 'Тут ваш заговолок' , #Загаловок
+    description = 'Тут ваше описание' , #Описание
     colour = discord.Colour.gold #Цвет ебмеда
     ) #Закрываем ембед
+    #Доп.Части ембеда
     embed.set_footer(text='Тут ваша строчка ебмеда') #Строчка ембеда
-    embed.set_image(url='https://img5.goodfon.ru/wallpaper/nbig/8/9c/abstraktsiia-fon-kartinka-sinii-goluboi-smeshenie-tsvetov-kr.jpg') #Сылка на картинку
-    embed.set_author(name='Тут имя автора')  # автор
-    icon_url='https://klike.net/uploads/posts/2019-03/medium/1551512888_2.jpg')  #Ава автора
+    embed.set_image(url='https://cdn.discordapp.com/attachments/710848456306065410/710879635168297010/1277_koichi.png') #Сылка на картинку
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/710848456306065410/710879917235241041/3802_corona_stare.png') #Сылка на картинку
+    embed.set_author(name='Тут имя автора') # автор
+    icon_url='https://cdn.discordapp.com/attachments/710848456306065410/710880495143092384/4109_tttgun.png' #Ава автора
+    embed.add_field(name='Заговолок поля', value='Тут то что внутре ебмеда', inline=False) #Добовления поля
+    embed.add_field(name='Заговолок поля', value='Тут то что внутре ебмеда', inline=False) #Добовления поля
     embed.add_field(name='Заговолок поля', value='Тут то что внутре ебмеда', inline=True) #Добовления поля
-    await ctx.send(embed=embed)  # Отпрвака ембед
+
+    await ctx.send(embed=embed) # Отпрвака ембед
 
   if page == '2':
     await ctx.send('123')
