d0a39943f8c7c617217f8b1e9a681ecabc65f4c8
Blockman_ <65733896+Blockman02135@users.noreply.github.com>
2020-05-22 11:30:33 +0300

    Main Bot

---
+++ EssentialsX.py
@@ -0,0 +1,22 @@
+﻿import discord
+from discord.ext import commands
+import asyncio
+import os
+
+Bot = commands.Bot(command_prefix='&')
+Bot.remove_command('help')
+
+@Bot.event
+async def on_ready():
+	await Bot.change_presence(activity= discord.Game(name= 'I am loaded!'))
+#Commands==========
+
+@Bot.command()
+async def hello(ctx):
+	await ctx.send('Hi')
+		
+token = os.environ.get('EX_TOKEN')
+
+Bot.run(str(token))
+	
+#Bot.run(open('token1.txt', 'r').read()) # Строка запуска бота
